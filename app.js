4bd065c3dd4b42d812081ce0b16e95ca3e6c2ec6
haha <lha3421@gmail.com>
2022-02-03 11:27:10 +0900

    2.4 Brush Size

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const canvas = document.querySelector("#jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.querySelector("#jsMode");
 
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
@@ -11,6 +13,7 @@ ctx.strokeStyle = "black";
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 // 조건문 선언
 
 // canvas 위에서 움직이는지 아닌지 확인
@@ -49,6 +52,27 @@ function handleColorClick(event) {
   ctx.strokeStyle = color;
 }
 
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+function handleModeClick(event) {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
+}
 Array.from(colors).forEach((color) =>
   color.addEventListener("click", handleColorClick)
 );
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
