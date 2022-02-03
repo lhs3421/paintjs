0e2d69db98571a60544cc7d06e9d352229cd41bb
haha <lha3421@gmail.com>
2022-02-03 12:09:02 +0900

    2.5 Filling Mode

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.querySelector("#jsMode");
 
+const INITIAL_COLOR = "black";
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
 
-ctx.strokeStyle = "black";
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -22,6 +24,7 @@ if (canvas) {
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
 }
 
 // 함수 선언
@@ -44,12 +47,20 @@ function stopPainting() {
 }
 
 function startPainting() {
-  painting = true;
+  if (filling === false) {
+    painting = true;
+  }
+}
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
 }
 
 function handleColorClick(event) {
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
 }
 
 function handleRangeChange(event) {
